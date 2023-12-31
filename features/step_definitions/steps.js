d052056d20d552254c871e9002f25cf5d0cb5075
md-hasan-ali <hasanaliringku@gmail.com>
2023-12-31 16:01:59 +0600

    Successfully passed 1 Scenario

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,18 +1,20 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
 
-function isItFriday(today) {
-    // We'll leave the implementation blank for now
-  }
-  
-  Given('today is Sunday', function () {
-    this.today = 'Sunday';
-  });
-  
-  When('I ask whether it\'s Friday yet', function () {
-    this.actualAnswer = isItFriday(this.today);
-  });
-  
-  Then('I should be told {string}', function (expectedAnswer) {
+function isItFriday(daysOfWeek) {
+    const today1 = new Date();
+    const dayOfWeekIndex = today1.getDay();
+    return daysOfWeek[dayOfWeekIndex];
+}
+
+Given('today is Sunday', function () {
+    this.daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+});
+
+When('I ask whether it\'s Friday yet', function () {
+    this.actualAnswer = isItFriday(this.daysOfWeek);
+});
+
+Then('I should be told {string}', function (expectedAnswer) {
     assert.strictEqual(this.actualAnswer, expectedAnswer);
-  });
+});
