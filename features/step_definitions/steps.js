11b0017780f7905d85ba515f9878af80620d3606
md-hasan-ali <hasanaliringku@gmail.com>
2023-12-31 17:15:49 +0600

    Using Veriabls and Examples

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,18 +1,45 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
 
-function isItFriday(daysOfWeek) {
-    const today1 = new Date();
-    const dayOfWeekIndex = today1.getDay();
-    return daysOfWeek[dayOfWeekIndex];
+// function isItFriday(daysOfWeek) {
+//     const today1 = new Date();
+//     const dayOfWeekIndex = today1.getDay();
+//     if (daysOfWeek[dayOfWeekIndex] === "Friday") {
+//         return "HASAN"
+//     } else {
+//         return "Nope!!!!!!!!"
+//     }
+//     // return daysOfWeek[dayOfWeekIndex];
+// }
+// Given('today is Sunday', function () {
+//     this.daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// });
+// When('I ask whether it\'s Friday yet', function () {
+//     this.actualAnswer = isItFriday(this.daysOfWeek);
+// });
+// Then('I should be told {string}', function (expectedAnswer) {
+//     if (this.actualAnswer === "Friday") {
+//         assert.strictEqual(this.actualAnswer, expectedAnswer);
+//     } else {
+//         console.log("Today is not Friday")
+//     }
+// });
+
+// Another Examples
+function isFriday(today) {
+    if (today === "Friday") {
+        return "TGIF";
+    } else {
+        return "Nope";
+    }
 }
 
-Given('today is Sunday', function () {
-    this.daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+Given('today is {string}', function (givenDay) {
+    this.today = givenDay;
 });
 
 When('I ask whether it\'s Friday yet', function () {
-    this.actualAnswer = isItFriday(this.daysOfWeek);
+    this.actualAnswer = isFriday(this.today);
 });
 
 Then('I should be told {string}', function (expectedAnswer) {
